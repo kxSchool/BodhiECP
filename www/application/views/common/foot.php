cc3b6282cd69720586914f991cfbfa5cf3f75be1
kxschool <28586585@qq.com>
2024-03-25 11:49:36 +0800

    qq

--- www/application/views/common/foot.php
+++ www/application/views/common/foot.php
@@ -23,7 +23,7 @@
 							<li>
 								<div class="floatleft bd"></div>
 								<div class="floatleft link-th">
-									<p class="tel">jacky588@qq.com</p>
+									<p class="tel">QQ:28586585</p>
 									<p>周一至周日8:00-22:00
 									</p>
 								</div>
@@ -50,7 +50,7 @@
 				</div>
 				<div class="footer-inner text-center">
 					<p class="copyright">苏ICP备15009296号 上海xx信息科技有限公司</p>
-					<p class="copyright">Powered by <a href="http://www.js715.com/2024/03/17/%e4%bc%81%e4%b8%9a%e6%9c%8d%e5%8a%a1%e5%b9%b3%e5%8f%b0/">章大牛计算机服务工作室</a></p>
+					<p class="copyright">Powered by <a href="http://www.js715.com/2024/03/17/%e4%bc%81%e4%b8%9a%e6%9c%8d%e5%8a%a1%e5%b9%b3%e5%8f%b0/">章大牛计算机工作室</a></p>
 				</div>
 			</div>
 
